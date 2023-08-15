27280b2e91dc7004e7cc20ebf0f28d925b5de426
kiranvinzoda <101656754+kiranvinzoda4@users.noreply.github.com>
2023-08-15 15:18:33 +0530

    fist commit

---
+++ pages/customer/index.js
@@ -0,0 +1,47 @@
+import * as React from "react";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import Deposits from "./Deposits";
+import Orders from "./Orders";
+import MainLayout from "../../layouts/main";
+import Title from "../Title";
+
+export default function Customers() {
+  return (
+    <MainLayout>
+      <Title>Customers</Title>
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={8} lg={9}>
+          <Paper
+            sx={{
+              p: 2,
+              display: "flex",
+              flexDirection: "column",
+              height: 240,
+            }}
+          >
+            <Deposits />
+          </Paper>
+        </Grid>
+        <Grid item xs={12} md={4} lg={3}>
+          <Paper
+            sx={{
+              p: 2,
+              display: "flex",
+              flexDirection: "column",
+              height: 240,
+            }}
+          >
+            <Deposits />
+          </Paper>
+        </Grid>
+        {/* Recent Orders */}
+        <Grid item xs={12}>
+          <Paper sx={{ p: 2, display: "flex", flexDirection: "column" }}>
+            <Orders />
+          </Paper>
+        </Grid>
+      </Grid>
+    </MainLayout>
+  );
+}
---
+++ pages/index.js
@@ -0,0 +1,23 @@
+import Dashboard from "./dashboard";
+import Login from "./auth/SignIn";
+import SignUp from "./auth/SignUp";
+import { useEffect, useState } from "react";
+
+export default function MainPage() {
+  const [token, setToken] = useState();
+  // useEffect(() => {
+  //   // Set a value in localStorage
+  //   localStorage.setItem("token", "myValue");
+  // }, []);
+
+  useEffect(() => {
+    setToken(localStorage.getItem("token"));
+  }, []);
+
+  if (token) {
+    console.log(token);
+    return <Dashboard />;
+  } else {
+    return <SignUp />;
+  }
+}
